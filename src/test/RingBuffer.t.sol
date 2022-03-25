c5c686a1fe7c58858ea0727dee468eb6442ec616
jmcph4 <jmcph4.github@gmail.com>
2022-03-25 15:23:25 +1000

    Add unit test for ring buffer insertion
    This commit adds a unit test case for insertion into an empty ring
    buffer.

--- src/test/RingBuffer.t.sol
+++ src/test/RingBuffer.t.sol
@@ -3,10 +3,34 @@ pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
 
+import "../RingBuffer.sol";
+
 contract RingBufferTest is DSTest {
     function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    function testPushNormalEmpty() public {
+        uint256 some_len = 12; /* arbitrary */
+        bytes32 some_elem = bytes32(0);
+        RingBuffer.RB_RingBuffer memory init_buf = RingBuffer.init(some_len);
+
+        RingBuffer.RB_RingBuffer memory actual_buf = RingBuffer.push(
+            init_buf,
+            some_elem
+        );
+
+        bytes32[] memory expected_xs = new bytes32[](1);
+        expected_xs[0] = some_elem;
+
+        RingBuffer.RB_RingBuffer memory expected_buf = RingBuffer.RB_RingBuffer(
+            expected_xs,
+            some_len,
+            1,
+            0
+        );
+
+        assertEq(actual_buf.xs[0], expected_buf.xs[0]);
+        assertEq(actual_buf.n, expected_buf.n);
+        assertEq(actual_buf.read, expected_buf.read);
+        assertEq(actual_buf.write, expected_buf.write);
     }
 }
