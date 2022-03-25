bd8144d4a9140373f9728a2daf2380dc76086735
jmcph4 <jmcph4.github@gmail.com>
2022-03-25 16:35:45 +1000

    Fix errors in test suite
    This commit fixes various build errors in the test suite.

--- src/test/RingBuffer.t.sol
+++ src/test/RingBuffer.t.sol
@@ -36,16 +36,17 @@ contract RingBufferTest is DSTest {
 
     function testPushNormalFull() public {
         uint256 some_len = 3; /* arbitrary */
-        bytes32 some_elems = new bytes32[](some_len);
-        some_elems.push(bytes32(1));
-        some_elems.push(bytes32(2));
-        some_elems.push(bytes32(3));
-        bytes32 some_extra_elem = bytes32(12);
+        bytes32[] memory some_elems = new bytes32[](some_len);
+        some_elems[0] = bytes32(uint256(1));
+        some_elems[1] = bytes32(uint256(2));
+        some_elems[2] = bytes32(uint256(3));
+        bytes32 some_extra_elem = bytes32(uint256(12));
+
         RingBuffer.RB_RingBuffer memory init_buf = RingBuffer.RB_RingBuffer(
             some_elems,
             some_len,
             0,
-            3,
+            3
         );
 
         RingBuffer.RB_RingBuffer memory actual_buf = RingBuffer.push(
