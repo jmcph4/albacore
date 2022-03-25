59839360f4f84aa3763a1d4de5f31c2592d7fe6a
jmcph4 <jmcph4.github@gmail.com>
2022-03-25 15:53:35 +1000

    Add unit test for insertion into full ring buffer
    This commit adds a unit test that handles the case of insertion into a
    full ring buffer.

--- src/test/RingBuffer.t.sol
+++ src/test/RingBuffer.t.sol
@@ -33,4 +33,43 @@ contract RingBufferTest is DSTest {
         assertEq(actual_buf.read, expected_buf.read);
         assertEq(actual_buf.write, expected_buf.write);
     }
+
+    function testPushNormalFull() public {
+        uint256 some_len = 3; /* arbitrary */
+        bytes32 some_elems = new bytes32[](some_len);
+        some_elems.push(bytes32(1));
+        some_elems.push(bytes32(2));
+        some_elems.push(bytes32(3));
+        bytes32 some_extra_elem = bytes32(12);
+        RingBuffer.RB_RingBuffer memory init_buf = RingBuffer.RB_RingBuffer(
+            some_elems,
+            some_len,
+            0,
+            3,
+        );
+
+        RingBuffer.RB_RingBuffer memory actual_buf = RingBuffer.push(
+            init_buf,
+            some_extra_elem
+        );
+
+        bytes32[] memory expected_xs = new bytes32[](some_len);
+        expected_xs[0] = some_extra_elem;
+        expected_xs[1] = some_elems[1];
+        expected_xs[2] = some_elems[2];
+
+        RingBuffer.RB_RingBuffer memory expected_buf = RingBuffer.RB_RingBuffer(
+            expected_xs,
+            some_len,
+            0,
+            1
+        );
+
+        assertEq(actual_buf.xs[0], expected_buf.xs[0]);
+        assertEq(actual_buf.xs[1], expected_buf.xs[1]);
+        assertEq(actual_buf.xs[2], expected_buf.xs[2]);
+        assertEq(actual_buf.n, expected_buf.n);
+        assertEq(actual_buf.read, expected_buf.read);
+        assertEq(actual_buf.write, expected_buf.write);
+    }
 }
